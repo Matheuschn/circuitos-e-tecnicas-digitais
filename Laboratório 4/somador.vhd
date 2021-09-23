3e3d320689825f8055f00efd7eceb222dd4b39ed
Matheus Schneider Camilo <matheus.schneider.camilo@gmail.com>
2021-09-22 23:39:32 -0300

    Adicionar arquivos do Laboratório 4

---
+++ Laboratório 4/somador.vhd
@@ -0,0 +1,17 @@
+LIBRARY IEEE;
+USE IEEE.Std_Logic_1164.ALL;
+USE IEEE.std_logic_unsigned.ALL;
+
+ENTITY somador IS
+	PORT (
+		A : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+		B : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+		F : OUT STD_LOGIC_VECTOR(3 DOWNTO 0)
+	);
+END somador;
+
+ARCHITECTURE arch OF somador IS
+BEGIN
+	F <= A + B;
+END arch;
+
